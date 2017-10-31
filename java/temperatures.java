dee03e06069c8b43f73bd7ef3a843c042a950199
Carlos Abraham <abraham@abraham.ml>
2017-10-31 15:04:33 -0400

    Update temperatures.java

--- java/temperatures.java
+++ java/temperatures.java
@@ -1,21 +1,23 @@
-/** 
-* 
-*  Java Temperatures Program using arrays 
-*/
+//  Java Temperatures Program using arrays 
+
 
 import java.util.ArrayList;
 
-public class Temperatures {
+public class TemperaturesB {
 	
 	public static void main(String[] args) {
 
-ArrayList<Integer> weeklyTemperatures = new ArrayList<Integer>();
+		ArrayList<Integer> weeklyTemperatures = new ArrayList<Integer>();
+		weeklyTemperatures.add(78);
+		weeklyTemperatures.add(67);
+		weeklyTemperatures.add(89);
+		weeklyTemperatures.add(94);
     
-    weeklyTemperatures.add(78);
-    weeklyTemperatures.add(67);
-    weeklyTemperatures.add(89);
-    weeklyTemperatures.add(94);
-    
-    System.out.println( weeklyTemperatures.get(1) );
+    weeklyTemperatures.add(2, 111);
+    System.out.println(weeklyTemperatures.get(3));
+
 	}
 }
+
+//Ouput: 89
+
