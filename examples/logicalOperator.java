f49e17a9470cdc6663b595707b6192bc3ac60702
go2max <max.uland1337@gmail.com>
2018-01-03 04:17:32 -0500

    Create logicalOperator.java

---
+++ examples/logicalOperator.java
@@ -0,0 +1,15 @@
+class apples {
+	public static void main(String args[]) {
+		
+		int boy, girl;
+		boy = 18;
+		girl = 40;
+		
+		if(boy > 10 || girl < 60) {
+			System.out.println("You can Enter");	
+		}
+		else {
+			System.out.println("You cannot Enter");
+		}
+	}
+}
