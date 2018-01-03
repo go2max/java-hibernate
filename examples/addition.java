f61255234144c7b5ddebdb8fb5a3635e59390e3d
go2max <max.uland1337@gmail.com>
2018-01-03 03:52:43 -0500

    Create addition.java

---
+++ examples/addition.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+class apples {
+	public static void main(String args[]) {
+		Scanner readFromline = new Scanner(System.in);
+		
+		int girls, boys, people;
+		girls = 13;
+		boys = 3;
+		people = girls + boys;
+		System.out.println(people);
+	
+	}
+}
