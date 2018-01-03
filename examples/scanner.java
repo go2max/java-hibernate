2e3e52f96eafa200008d40ec692f44ad93a8ad2e
go2max <max.uland1337@gmail.com>
2018-01-03 02:33:28 -0500

    Create scanner.java

---
+++ examples/scanner.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+class apples {
+	public static void main(String args[]) {
+  
+		System.out.print("Enter your name: ");
+    Scanner scanFromKeyboard = new Scanner(System.in);
+		System.out.println("Your name is " + scanFromKeyboard.nextLine());
+	}
+}
