3b7f9f40ff36643879ea7dbfa7bcd6b0fe62cfe4
go2max <max.uland1337@gmail.com>
2018-01-14 12:10:15 -0500

    Update JavaOutputFormatting.java

--- hackerrank/JavaOutputFormatting.java
+++ hackerrank/JavaOutputFormatting.java
@@ -1,1 +1,21 @@
-a
+import java.util.Scanner;
+
+public class Solution {
+
+    public static void main(String[] args) {
+            
+        Scanner sc=new Scanner(System.in);
+        
+            System.out.println("================================");
+            for(int i=0;i<3;i++){
+                
+                String s = sc.next();
+                int x =sc.nextInt();
+                
+                
+                System.out.printf("%-14s %03d %n", s, x);
+            }
+            System.out.println("================================");
+
+    }
+}
