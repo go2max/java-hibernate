8194f417bb7d36d02e475e774ec793c7e68b5b64
go2max <max.uland1337@gmail.com>
2018-02-12 00:57:25 -0500

    Create Colors.java

---
+++ utils/Colors.java
@@ -0,0 +1,33 @@
+/**
+ *
+ * @author Carlos Abraham
+ * www.19cah.com
+ * @19cah
+ */
+public class Colors {
+
+    public static final String RESET = "\u001B[0m";
+    public static final String BLACK = "\u001B[30m";
+    public static final String RED = "\u001B[31m";
+    public static final String GREEN = "\u001B[32m";
+    public static final String YELLOW = "\u001B[33m";
+    public static final String BLUE = "\u001B[34m";
+    public static final String PURPLE = "\u001B[35m";
+    public static final String CYAN = "\u001B[36m";
+    public static final String WHITE = "\u001B[37m";
+    
+    public static void main(String[] args) {
+      
+      System.out.println(RED+"Red");
+      System.out.println(GREEN+"Green");
+      System.out.println(BLACK+"Black");
+      System.out.println(YELLOW+"Yellow");
+      System.out.println(BLUE+"Blue");
+      System.out.println(PURPLE+"Purple");
+      System.out.println(CYAN+"Cyan");
+      System.out.println(WHITE+"White");
+      System.out.println(RESET+"Back to normal");
+      
+    }
+    
+}
